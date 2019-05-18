c687d5bf7346deab9d30be623380a5160304c025
Zac Svoboda <svobodaz@VT-8536.local>
2019-05-17 22:11:10 -0400

    404 route

---
+++ src/pages/404.vue
@@ -0,0 +1,10 @@
+<template>
+  <Layout>
+    <h1>404</h1>
+    <p>That link wasn't very good. Maybe it never worked but maybe I moved something. Who's to say?</p>
+  </Layout>
+</template>
+
+<script>
+export default {}
+</script>
