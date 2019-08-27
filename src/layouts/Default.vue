6688888ba457d782c55c544620333c5aa96ad979
Zachary Svoboda <svobodaz@orvis.com>
2019-08-26 21:35:01 -0400

    Update packages

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -17,8 +17,8 @@
 <style>
 body {
   font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
+  margin: 0;
+  padding: 0;
   line-height: 1.5;
 }
 
