9d5342a5b6080f713bae1e0519cbce0007447402
Zac <cczac1@gmail.com>
2019-10-13 15:41:18 -0400

    Fix up deprecated config

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,13 +1,27 @@
+const path = require('path')
+
+function addStyleResource (rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/theme/base.scss'),
+      ],
+    })
+}
+
 module.exports = {
   siteName: 'Zachary Svoboda',
   siteUrl: 'https://www.zacharysvoboda.com',
+  templates: {
+    BlogPost: '/writing/:slug'
+  },
   plugins: [
     {
       use: '@gridsome/source-filesystem',
       options: {
         path: './blog/**/*.md',
-        typeName: 'BlogPost',
-        route: '/:slug'
+        typeName: 'BlogPost'
       }
     },
     {
@@ -32,5 +46,11 @@ module.exports = {
         '@gridsome/remark-prismjs'
       ]
     }
-  }
+  },
+  chainWebpack (config) {
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+    types.forEach(type => {
+      addStyleResource(config.module.rule('scss').oneOf(type))
+    })
+	}
 }
