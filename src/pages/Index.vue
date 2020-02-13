5aa785992f6ca0b60fcd81d5a91a6a8f4bc4556c
Zachary Svoboda <cczac1@gmail.com>
2020-02-13 08:50:23 -0500

    chore(package): missing peer dependency

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,31 @@
 <template>
   <Layout>
-    <header>Hello! I'm Zac Svoboda</header>
-    
+    <canvas id="image-canvas"></canvas>
   </Layout>
 </template>
 
 <script>
 export default {
+  mounted() {
+    const image = new Image();
+    const canvas = document.getElementById('image-canvas');
+    const ctx = canvas.getContext('2d');
+
+    image.onload = () => {
+      ctx.drawImage(image, 33, 71, 104, 124, 21, 20, 87, 104);
+    };
+    image.src = '/images/me.jpg';
+
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+#myImageElementInTheDom {
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+}
 </style>
