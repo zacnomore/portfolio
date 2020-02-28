ef838c60b5b1152adb1f7c7be0841d80bfb5071c
Zac <cczac1@gmail.com>
2020-02-28 13:42:53 -0500

    Glitch homepage (#11)
    * Shrink image for performance
    
    * Fix format
    
    * Utilize glitch canvas

---
+++ src/components/GlitchImage.vue
@@ -0,0 +1,61 @@
+<template>
+  <canvas class="glitch"/>
+</template>
+
+<script>
+import glitch from 'glitch-canvas';
+export default {
+  props: ['imageURL'],
+  mounted: function() {
+    const canvas = document.querySelector(".glitch");
+    const ctx = canvas.getContext("2d");
+
+    function drawNormal() {
+      ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, canvas.width, canvas.height);
+    }
+
+    const glitchTime = 500;
+    const waitTime = 5000;
+    function draw(startMillis) {
+      if(performance.now() > startMillis + glitchTime) {
+        drawNormal();
+        console.log('Stop glitching');
+        setTimeout(() => {
+          console.log('Start glitching')
+          draw(performance.now());
+        }, waitTime);
+      } else {
+        const seed = Math.floor(Math.random() * (99 - 1) + 1);
+        glitch({amount: 10, interations: 10, quality: 99, seed})
+          .fromImage(image)
+          .toImage()
+          .then(image => {
+            ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, canvas.width, canvas.height);
+            requestAnimationFrame(() => {
+              draw(startMillis);
+            })
+          })
+          .catch(e => {
+            drawNormal();
+          });
+      }
+
+    }
+
+
+    const image = new Image();
+    image.src = this.imageURL;
+    image.onload = () => {
+      drawNormal();
+      draw(performance.now());
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .glitch {
+    width: 100%;
+    height: 100%;
+  }
+</style>
